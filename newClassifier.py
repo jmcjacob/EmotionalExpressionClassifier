ef17014bbe3f7cd5454e1eaf20961dd95f78c24a
Jacob Carse <jacob.carse@gmail.com>
2017-06-24 19:22:51 +0100

    New Classifier using TFLearn

---
+++ newClassifier.py
@@ -0,0 +1,106 @@
+import main
+import time
+import tflearn
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tflearn.callbacks import Callback
+from sklearn.metrics import classification_report
+from keras.layers.local import LocallyConnected2D
+
+
+class MonitorCallback(Callback):
+	def __init__(self, args, start_time):
+		super(self.__class__, self).__init__()
+		self.args = args
+		self.start = start_time
+		self.log = args.model_name
+
+	def on_epoch_end(self, state):
+		main.log(self.args, '{:.5f}s Epoch '.format(time.clock() - self.start) + str(state.epoch).zfill(4) +
+				 ' Loss = {:.5f}'.format(state.global_loss) + ' Accuracy = {:.5}'.format(state.global_acc))
+
+	def on_train_end(self, state):
+		main.log(self.args, '\n{:.5f}s'.format(time.clock() - self.start) + ' Validation Loss = {:.5f}'.format(state.global_loss) +
+				 ' Validation Accuracy = {:.5f}'.format(state.global_acc))
+
+
+class Classifier:
+	def __init__(self, args, start_time, num_classes, save_path, input_shape, scope_name, local=False):
+		tf.reset_default_graph()
+		self.args = args
+		self.scope_name = scope_name
+		self.start_time = start_time
+		self.save_path = self.args.resource_dir + self.args.model_name + self.scope_name
+		network = self.build_network(num_classes, input_shape, local)
+		self.model = tflearn.DNN(network, checkpoint_path=self.save_path, max_checkpoints=1, tensorboard_verbose=3, tensorboard_dir=self.save_path + 'tensorboard')
+
+	def load(self):
+		self.model.load(self.save_path + 'model.model')
+
+	def build_network(self, num_classes, input_shape, local):
+		network = tflearn.input_data(shape=[None, input_shape[0], input_shape[1], input_shape[2]])
+		if local:
+			local = LocallyConnected2D(64, 7, strides=(2, 2), activation='relu', use_bias=True, kernel_initializer='random_normal', bias_initializer='random_normal')
+			local.build(network.get_shape().as_list())
+			conv_1 = local.call(network)
+		else:
+			conv_1 = tflearn.relu(tflearn.conv_2d(network, 64, 7, strides=2, bias=True, padding='VALID', activation=None, name='Conv2d_1'))
+		maxpool_1 = tflearn.batch_normalization(tflearn.max_pool_2d(conv_1, 3, strides=2, padding='VALID', name='MaxPool_1'))
+
+		conv_2a = tflearn.relu(tflearn.conv_2d(maxpool_1, 96, 1, strides=1, padding='VALID', name='Conv_2a_FX1'))
+		maxpool_2a = tflearn.max_pool_2d(maxpool_1, 3, strides=1, padding='VALID', name='MaxPool_2a_FX1')
+		conv_2b = tflearn.relu(tflearn.conv_2d(conv_2a, 208, 3, strides=1, padding='VALID', name='Conv_2b_FX1'))
+		conv_2c = tflearn.relu(tflearn.conv_2d(maxpool_2a, 64, 1, strides=1, padding='VALID', name='Conv_2c_FX1'))
+		FX1_out = tflearn.merge([conv_2b, conv_2c], mode='concat', axis=3, name='FX1_out')
+
+		conv_3a = tflearn.relu(tflearn.conv_2d(FX1_out, 96, 1, strides=1, padding='VALID', name='Conv_3a_FX2'))
+		maxpool_3a = tflearn.max_pool_2d(FX1_out, 3, strides=1, padding='VALID', name='MaxPool_3a_FX2')
+		conv_3b = tflearn.relu(tflearn.conv_2d(conv_3a, 208, 3, strides=1, padding='VALID', name='Conv_3b_FX2'))
+		conv_3c = tflearn.relu(tflearn.conv_2d(maxpool_3a, 64, 1, strides=1, padding='VALID', name='Conv_3c_FX2'))
+		FX2_out = tflearn.merge([conv_3b, conv_3c], mode='concat', axis=3, name='FX2_out')
+		net = tflearn.flatten(FX2_out)
+		return tflearn.fully_connected(net, num_classes, activation='softmax')
+
+	def train(self, training_data, testing_data):
+		x, y = [m[0] for m in training_data], [n[1] for n in training_data]
+		monitor = MonitorCallback(self.args, self.start_time)
+		self.model.fit(x, y, n_epoch=self.args.epochs, validation_set=0.1, shuffle=True, show_metric=True, batch_size=self.args.batch_size, snapshot_step=2000, snapshot_epoch=True,
+					   run_id=self.args.model_name, callbacks=monitor)
+		main.log(self.args, '{:.5f}s Epoch '.format(time.clock() - self.start_time) + str(self.count_trainable_vars()) + ' trainable parameters')
+		self.model.save(self.save_path + 'model.model')
+		predictions, labels = self.evaluate(testing_data)
+		self.confusion_matrix(self.args, predictions, labels)
+
+	def classify(self, data):
+		return self.model.predict(data)
+
+	def evaluate(self, testing_data):
+		x, y = [m[0] for m in testing_data], [n[1] for n in testing_data]
+		return self.model.predict(x), y
+
+	@staticmethod
+	def count_trainable_vars():
+		total_parameters = 0
+		for variable in tf.trainable_variables():
+			shape = variable.get_shape()
+			variable_parametes = 1
+			for dim in shape:
+				variable_parametes *= dim.value
+			total_parameters += variable_parametes
+		return total_parameters
+
+	@staticmethod
+	def confusion_matrix(args, y_pred, labels):
+		y_actu = np.zeros(len(y_pred))
+		for i in range(len(labels)):
+			for j in range(len(labels[i])):
+				if labels[i][j] == 1.00:
+					y_actu[i] = j
+
+		p_labels = pd.Series(y_pred)
+		t_labels = pd.Series(y_actu)
+		df_confusion = pd.crosstab(t_labels, p_labels, rownames=['Actual'], colnames=['Predicted'], margins=True)
+
+		main.log(args, df_confusion)
+		main.log(args, classification_report(y_actu, y_pred))
--- train.py
+++ train.py
@@ -2,57 +2,56 @@ import os
 import cv2
 import main
 import time
-import tflearn
 import numpy as np
-import tensorflow as tf
 from classifier import Classifier
+from newClassifier import Classifier as newClassifier
 
 
 def train_2(args):
+	start_time = time.clock()
+	main.log(args, '\n---------- RGB, No Frontalization, Convolutional Layers ----------')
+	train_data = retrieve_data(args.training_dir + '/rgb')
+	test_data = retrieve_data(args.testing_dir + '/rgb')
+	main.log(args, '{:.5f}'.format(time.clock() - start_time) + 's ' + str(len(train_data)) + ' images read across ' + str(len(train_data[0][1])) + ' classes')
 
-	# Load data
-	data = retrieve_data(args.training_dir + '/rgb')
-	data += retrieve_data(args.testing_dir + '/rgb')
-	x, y = [m[0] for m in data], [n[1] for n in data]
+	rgb_classifier = newClassifier(args, start_time, len(train_data[0][1]), args.resource_dir, train_data[0][0].shape, '/rgb/', local=False)
+	rgb_classifier.train(train_data, test_data)
 
-	# Define number of output classes.
-	num_classes = 7
+	main.log(args, '\n---------- LBP, No Frontalization, Convolutional Layers ----------')
+	train_data = retrieve_data(args.training_dir + '/lbp')
+	test_data = retrieve_data(args.testing_dir + '/lbp')
+	main.log(args, str(time.clock() - start_time) + 's ' + str(len(train_data)) + ' images read across ' + str(len(train_data[0][1])) + ' classes')
 
-	# Define padding scheme.
-	padding = 'VALID'
+	lbp_classifier = newClassifier(args, start_time, len(train_data[0][1]), args.resource_dir, train_data[0][0].shape, '/lbp/', local=False)
+	lbp_classifier.train(train_data, test_data)
 
-	# Model Architecture
-	network = tflearn.input_data(shape=[None, 224, 224, 1])
-	conv_1 = tflearn.relu(tflearn.conv_2d(network, 64, 7, strides=2, bias=True, padding=padding, activation=None, name='Conv2d_1'))
-	maxpool_1 = tflearn.batch_normalization(tflearn.max_pool_2d(conv_1, 3, strides=2, padding=padding, name='MaxPool_1'))
+	main.log(args, '\n---------- RGB, Frontalization, Convolutional Layers ----------')
+	train_data = retrieve_data(args.training_dir + '/frgb')
+	test_data = retrieve_data(args.testing_dir + '/frgb')
+	main.log(args, '{:.5f}'.format(time.clock() - start_time) + 's ' + str(len(train_data)) + ' images read across ' + str(len(train_data[0][1])) + ' classes')
 
-	# FeatEX-1
-	conv_2a = tflearn.relu(tflearn.conv_2d(maxpool_1, 96, 1, strides=1, padding=padding, name='Conv_2a_FX1'))
-	maxpool_2a = tflearn.max_pool_2d(maxpool_1, 3, strides=1, padding=padding, name='MaxPool_2a_FX1')
-	conv_2b = tflearn.relu(tflearn.conv_2d(conv_2a, 208, 3, strides=1, padding=padding, name='Conv_2b_FX1'))
-	conv_2c = tflearn.relu(tflearn.conv_2d(maxpool_2a, 64, 1, strides=1, padding=padding, name='Conv_2c_FX1'))
-	FX1_out = tflearn.merge([conv_2b, conv_2c], mode='concat', axis=3, name='FX1_out')
-	# FeatEX-2
-	conv_3a = tflearn.relu(tflearn.conv_2d(FX1_out, 96, 1, strides=1, padding=padding, name='Conv_3a_FX2'))
-	maxpool_3a = tflearn.max_pool_2d(FX1_out, 3, strides=1, padding=padding, name='MaxPool_3a_FX2')
-	conv_3b = tflearn.relu(tflearn.conv_2d(conv_3a, 208, 3, strides=1, padding=padding, name='Conv_3b_FX2'))
-	conv_3c = tflearn.relu(tflearn.conv_2d(maxpool_3a, 64, 1, strides=1, padding=padding, name='Conv_3c_FX2'))
-	FX2_out = tflearn.merge([conv_3b, conv_3c], mode='concat', axis=3, name='FX2_out')
-	net = tflearn.flatten(FX2_out)
-	loss = tflearn.fully_connected(net, num_classes, activation='softmax')
+	frgb_classifier = newClassifier(args, start_time, len(train_data[0][1]), args.resource_dir, train_data[0][0].shape, '/frgb/', local=False)
+	frgb_classifier.train(train_data, test_data)
 
-	# Compile the model and define the hyperparameters
-	network = tflearn.regression(loss, optimizer='Adam',loss='categorical_crossentropy',learning_rate=0.0001)
+	main.log(args, '\n---------- RGB, Frontalization, Locally Connected Layers ----------')
+	lfrgb_classifier = newClassifier(args, start_time, len(train_data[0][1]), args.resource_dir, train_data[0][0].shape, '/lfrgb/', local=True)
+	lfrgb_classifier.train(train_data, test_data)
 
-	# Final definition of model checkpoints and other configurations
-	model = tflearn.DNN(network, checkpoint_path=args.resource_dir + args.model_name,max_checkpoints=1, tensorboard_verbose=2, tensorboard_dir=args.log)
+	main.log(args, '\n---------- LBP, Frontalization, Convolutional Layers ----------')
+	train_data = retrieve_data(args.training_dir + '/flbp')
+	test_data = retrieve_data(args.testing_dir + '/flbp')
+	main.log(args, '{:.5f}'.format(time.clock() - start_time) + 's ' + str(len(train_data)) + ' images read across ' + str(len(train_data[0][1])) + ' classes')
 
-	# Fit the model, train for 20 epochs. (Change all parameters to flags (arguments) on version 2.)
-	model.fit(x, y, n_epoch=20, validation_set=0.1, shuffle=True, show_metric=True, batch_size=350, snapshot_step=2000, snapshot_epoch=True, run_id=args.model_name)
+	flbp_classifier = newClassifier(args, start_time, len(train_data[0][1]), args.resource_dir, train_data[0][0].shape, '/flbp/', local=False)
+	flbp_classifier.train(train_data, test_data)
 
-	# Save the model
-	model.save(args.resource_dir + args.model_name + 'thing.model')
+	main.log(args, '\n---------- LBP, Frontalization, Locally Connected Layers ----------')
+	lflbp_classifier = newClassifier(args, start_time, len(train_data[0][1]), args.resource_dir, train_data[0][0].shape, '/lflbp/', local=True)
+	lflbp_classifier.train(train_data, test_data)
 
+	main.log(args, '\n Completed Training in ' + str(time.clock() - start_time) + 's\n')
+
+	avg_accuracy_2(args, start_time)
 
 def train(args):
 	start_time = time.clock()
@@ -154,3 +153,29 @@ def avg_accuracy(args, start_time):
 		prediction.append(np.argmax(votes[i]))
 
 	Classifier.confusion_matrix(args, prediction, labels)
+
+def avg_accuracy_2(args, start_time):
+	rgb, lbp = retrieve_data(args.testing_dir + '/rgb'), retrieve_data(args.testing_dir + '/lbp')
+	frgb, flbp = retrieve_data(args.testing_dir + '/frgb'), retrieve_data(args.testing_dir + '/flbp')
+	labels, predictions = np.zeros(0), []
+
+	results = newClassifier(args, start_time, len(rgb[0][1]), args.resource_dir, rgb[0][0].shape, '/rgb/', local=False).classify(rgb)
+	predictions.append(results[1])
+	labels = results[0]
+	predictions.append(newClassifier(args, start_time, len(rgb[0][1]), args.resource_dir, rgb[0][0].shape, '/lbp/', local=False).classify(lbp)[1])
+	predictions.append(newClassifier(args, start_time, len(rgb[0][1]), args.resource_dir, rgb[0][0].shape, '/frgb/', local=False).classify(frgb)[1])
+	predictions.append(newClassifier(args, start_time, len(rgb[0][1]), args.resource_dir, rgb[0][0].shape, '/lfrgb/', local=True).classify(frgb)[1])
+	predictions.append(newClassifier(args, start_time, len(rgb[0][1]), args.resource_dir, rgb[0][0].shape, '/flbp/', local=False).classify(flbp)[1])
+	predictions.append(newClassifier(args, start_time, len(rgb[0][1]), args.resource_dir, rgb[0][0].shape, '/lflbp/', local=True).classify(flbp)[1])
+
+	votes = np.zeros((len(predictions[0]), len(labels[0])))
+	for i in range(len(predictions)):
+		for j in range(len(predictions[0])):
+			vote = predictions[i][j]
+			votes[j][int(vote)] += 1.0
+
+	prediction = []
+	for i in range(len(votes)):
+		prediction.append(np.argmax(votes[i]))
+
+	newClassifier.confusion_matrix(args, prediction, labels)
